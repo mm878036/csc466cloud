8b9b3e3c4894c0ad2dff60464a8851a08c66ee8a
mm878036 <54553960+mm878036@users.noreply.github.com>
2020-11-20 12:06:24 -0500

    Update install_mpi.sh

--- install_mpi.sh
+++ install_mpi.sh
@@ -4,12 +4,14 @@ sudo yum -y group install "Development Tools"
 sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
 sudo tar xzf openmpi-3.1.2.tar.gz
 cd openmpi-3.1.2
-sudo ./configure --prefix=/opt/openmpi/3.1.2
+sudo ./configure --prefix=/software/openmpi/3.1.2 --enable-orterun-prefix-by-default
 sudo make
 sudo make all install
 
-echo "export PATH=$PATH:/opt/openmpi/3.1.2/bin" | sudo tee -a /etc/profile 
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/openmpi/3.1.2/lib/" | sudo tee -a /etc/profile 
-cd ..
+while IFS= read -r line; do
+  echo 'export PATH=$PATH:/software/openmpi/3.1.2/bin' | sudo tee -a /users/$i/.bashrc
+  echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/' | sudo tee -a /users/$i/.bashrc
+done < <( ls -l /users | grep rwx | cut -d' ' -f3 )
+
 sudo rm -Rf openmpi-3.1.2
-sudo rm -Rf openmpi-3.1.2.tar.gz 
+sudo rm -Rf openmpi-3.1.2.tar.gz
